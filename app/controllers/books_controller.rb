4ffe18c14b2fbe2feb58d5a957766342a50ccc2d
EC2 Default User <ec2-user@ip-172-31-5-116.ec2.internal>
2022-01-24 21:32:25 +0000

    [Add]保存機能を追加した

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,12 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+  
+  def create
+    book = Book.new
+    book.save
+    redirect_to'/books/show'
   end
 
   def index
@@ -10,4 +17,11 @@ class BooksController < ApplicationController
 
   def edit
   end
+  
+  private
+  
+  def list_params
+    params.require(:book).permit(:title,:body)
+  end
+
 end
