46a20e99bdcc1cec946483fcc173a60ba5ab2d30
EC2 Default User <ec2-user@ip-172-31-5-116.ec2.internal>
2022-02-05 13:38:09 +0000

    [Finish]showページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   get 'books/new'
   post 'books' => 'books#create'
-  get 'books' => 'books#index'
+  get 'index' => 'books#index'
   get 'books/:id' => 'books#show' ,as: 'book'
-  get 'books/edit'
+  get 'books/edit/:id' => 'books#edit' ,as: 'edit_list'
   get 'homes/top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
